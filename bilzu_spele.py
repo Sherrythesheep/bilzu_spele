f0147ce2a29e7f3d7a683168f1ddf2e500613e79
Sherrythesheep <evelinapuga07@gmail.com>
2023-12-13 18:29:44 +0200

    Update bilzu_spele.py

--- bilzu_spele.py
+++ bilzu_spele.py
@@ -1,7 +1,7 @@
 from tkinter import*
 from PIL import ImageTk, Image
-import random
-from tkinter import messagebox
+#import random
+#from tkinter import messagebox
 gameWindow=Tk()
 gameWindow.title("Vienādie attēli")
 
@@ -18,26 +18,26 @@ bgImg=ImageTk.PhotoImage(Image.open("ruksis.jpg"))
 bgImg=ImageTk.PhotoImage(Image.open("suns.jpg"))
 bgImg=ImageTk.PhotoImage(Image.open("zirgs.jpg"))
 
-btn0=Button(width=4,height=4,Image=bgImg)
-btn1=Button(width=4,height=4,Image=bgImg)
-btn2=Button(width=4,height=4,Image=bgImg)
-btn4=Button(width=4,height=4,Image=bgImg)
-btn5=Button(width=4,height=4,Image=bgImg)
-btn6=Button(width=4,height=4,Image=bgImg)
-btn7=Button(width=4,height=4,Image=bgImg)
-btn8=Button(width=4,height=4,Image=bgImg)
-btn9=Button(width=4,height=4,Image=bgImg)
+btn0=Button(width=10,height=10,Image=bgImg)
+btn1=Button(width=10,height=10,Image=bgImg)
+btn2=Button(width=10,height=10,Image=bgImg)
+btn4=Button(width=10,height=10,Image=bgImg)
+btn5=Button(width=10,height=10,Image=bgImg)
+btn6=Button(width=10,height=10,Image=bgImg)
+btn7=Button(width=10,height=10,Image=bgImg)
+btn8=Button(width=10,height=10,Image=bgImg)
+btn9=Button(width=10,height=10,Image=bgImg)
 
-btn0=Button(width=8,height=10)
-btn1=Button(width=8,height=10)
-btn2=Button(width=8,height=10)
-btn3=Button(width=8,height=10)
-btn4=Button(width=8,height=10)
-btn5=Button(width=8,height=10)
-btn6=Button(width=8,height=10)
-btn7=Button(width=8,height=10)
-btn8=Button(width=8,height=10)
-btn9=Button(width=8,height=10)
+btn0=Button(width=10,height=10)
+btn1=Button(width=10,height=10)
+btn2=Button(width=10,height=10)
+btn3=Button(width=10,height=10)
+btn4=Button(width=10,height=10)
+btn5=Button(width=10,height=10)
+btn6=Button(width=10,height=10)
+btn7=Button(width=10,height=10)
+btn8=Button(width=10,height=10)
+btn9=Button(width=10,height=10)
 
 btn0.grid(row=0, column=0)
 btn1.grid(row=0, column=1)
