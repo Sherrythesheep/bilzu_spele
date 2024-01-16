2bf36ce440df5ab75dff336590dbe8e0f13d963c
Sherrythesheep <123467157+Sherrythesheep@users.noreply.github.com>
2024-01-16 10:08:06 +0200

    Update bilzu_spele.py

--- bilzu_spele.py
+++ bilzu_spele.py
@@ -1,37 +1,52 @@
-from tkinter import*
+from tkinter import *
+from tkinter import messagebox
 from PIL import ImageTk, Image
 import random
-from tkinter import messagebox
-
+import time
 
 gameWindow=Tk()
 gameWindow.title("Vienādie attēli")
-count=0 #cik rutinas atvertas
-correctAnswers=0 #skaita pareizās atbildes
-answers=[]#saraksts ar atbildem , define tiksu sarakstu un speles laika tas tiek aizpildits
-answer_dict={}#kas ur piespiests, salidzinas ar atteliem no saraksta
+openCardsCount=0 #cik rutinas atvertas
+correctAnswers=0 #skaita uzminēto attēlu komplektu skaitu
+openCards=[] #saraksts ar atvertajam kartinam (viena vai divas)
+answer_dict={} #kas ir piespiests, salidzinas ar atteliem no saraksta
 
 def btnClick(btn,number):
-    global count, correctAnswers, answers, answer_dict
-    if btn["image"]=="pyimage6" and count<2:
-        btn["image"]=ImageList[number]
-        count+=1#viena rūtiņa atklāta
-        answers.append(number)#pievieno pie atbildēm
+    
+    global openCardsCount, openCards, answer_dict, correctAnswers
+    
+    if btn["image"] == "pyimage6" and openCardsCount < 2:
+        btn["image"] = ImageList[number]
+        openCardsCount += 1 #viena jauna rūtiņa atklāta
+        openCards.append(number) #pievieno kartinas numuru pie atbildēm
         answer_dict[btn]=ImageList[number]
-    if len(answers)==2:#ja atvertas 2 kartites
-        if ImageList[answers[0]]==ImageList[answers[1]]:
+    
+    if len(openCards) == 2:#ja atvertas 2 kartites
+        
+        if ImageList[openCards[0]]==ImageList[openCards[1]]:
             for key in answer_dict:
                 key["state"]=DISABLED
-        if correctAnswers==2:
-            messagebox.showinfo("Vienādi attēli", "Esi uzminējis!")
-    else: 
-        messagebox.showinfo("Vienādi attēli","Neuzminēji!")
-        for key in answer_dict:
-            key["image"]="pyimage6"
-            count=0
-            answers=[]
-            answer_dict={}
+
+            openCards = []
+            answer_dict = {}
+            openCardsCount = 0
+            correctAnswers += 1 #palielinam uzminēto attēlu komplektu skaitu
+            messagebox.showinfo("Vienādi attēli", "Esi uzminējis! Kopā uzminēti komplekti: " + str(correctAnswers))
+        else :
+            Tk.update(btn)
+            time.sleep(0.5)
+            for key in answer_dict:
+                key["image"] = "pyimage6"
+            openCardsCount = 0;
+            openCards = []
+            answer_dict = {}
+            #messagebox.showinfo("Vienādi attēli","Neuzminēji!")
+
+    if correctAnswers == 4:
+        messagebox.showinfo("Apsveicam!", "Spēle pabeigta")
+
     return 0
+
 
 #attēli
 myImg1=ImageTk.PhotoImage(Image.open("kakis.jpg").resize((180,180)))
@@ -45,8 +60,8 @@ bgImg=ImageTk.PhotoImage(Image.open("kepa.jpg").resize((180,180)))
 
 btn0=Button(width=180,height=180,image=bgImg, command=lambda:btnClick(btn0,0))
 btn1=Button(width=180,height=180,image=bgImg, command=lambda:btnClick(btn1,1))
-btn3=Button(width=180,height=180,image=bgImg, command=lambda:btnClick(btn2,2))
-btn2=Button(width=180,height=180,image=bgImg, command=lambda:btnClick(btn3,3))
+btn2=Button(width=180,height=180,image=bgImg, command=lambda:btnClick(btn2,2))
+btn3=Button(width=180,height=180,image=bgImg, command=lambda:btnClick(btn3,3))
 btn4=Button(width=180,height=180,image=bgImg, command=lambda:btnClick(btn4,4))
 btn5=Button(width=180,height=180,image=bgImg, command=lambda:btnClick(btn5,5))
 btn6=Button(width=180,height=180,image=bgImg, command=lambda:btnClick(btn6,6))
